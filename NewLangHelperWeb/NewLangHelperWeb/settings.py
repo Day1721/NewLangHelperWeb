ecdffc0e510a583ab6e43cad87d7e436ab523cc7
Marcin Wawerka <marcin.wawerka@gmail.com>
2017-04-03 19:47:35 +0200

    Change default DB to PostgreSQL

--- NewLangHelperWeb/NewLangHelperWeb/settings.py
+++ NewLangHelperWeb/NewLangHelperWeb/settings.py
@@ -79,9 +79,18 @@ WSGI_APPLICATION = 'NewLangHelperWeb.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST':'mydbinstance.cifdsdjfxsq4.eu-central-1.rds.amazonaws.com',
+        'PORT':'5432',
+        'NAME': 'newlanghelperdb',
+        'USER': 'root',
+        'PASSWORD' : 'dbpassword234',
+    },
+    
+    'dbsqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 
