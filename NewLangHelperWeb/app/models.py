11aedb943e10970cc4766a9888a1daa0d3cd0165
Marcin Wawerka <marcin.wawerka@gmail.com>
2017-04-03 19:43:51 +0200

    Add base models

--- NewLangHelperWeb/app/models.py
+++ NewLangHelperWeb/app/models.py
@@ -3,5 +3,24 @@ Definition of models.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class AppUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # tu pojawi się to co będzie potrzebne o użytkownikach
+
+
+class WordCard(models.Model):
+    first_word = models.CharField(max_length=100)
+    second_word = models.CharField(max_length=100)
+
+    #wstepnie to CharFieldy, potem można zrobić jakiś wybór z listy języków
+    first_language = models.CharField(max_length=100)
+    second_language = models.CharField(max_length=100)
+
+    # każda fiszka na razie należy do jednego użytkownika
+    user = models.ForeignKey(AppUser)
+
