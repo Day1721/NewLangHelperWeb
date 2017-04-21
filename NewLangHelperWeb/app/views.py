c7245c21054909c2f8f15ca8dca5fd1e480ccecd
Marcin Wawerka <marcin.wawerka@gmail.com>
2017-04-21 23:31:39 +0200

    Change views to reflect REST API

--- NewLangHelperWeb/app/views.py
+++ NewLangHelperWeb/app/views.py
@@ -3,7 +3,7 @@ Definition of views.
 """
 
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.template import RequestContext
 from datetime import datetime
 from rest_framework import status
@@ -57,32 +57,49 @@ from .models import *
 from rest_framework import viewsets
 from .serializers import *
 from django.views.decorators.http import require_POST, require_GET, require_http_methods
-from .DBHandler.DBHandler import get_words_from_user_group
+from .DBHandler import DBHandler
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+from django.http import HttpResponseRedirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+class GroupList(APIView):
+    def get(self, request, format=None):
+        print("here2")
+        groups = DBHandler.get_groups_from_user(request.user)
+        serializer = GroupSerializer(groups, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = GroupSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@require_GET
-def card(request):
-    user1 = request.user
-    if user1.is_authenticated:
-        group1 = request.context["group_name"]
-        group_words = get_words_from_user_group(user1, group1)
-        for word in group_words:
-            serializer = WordSerializer(word)
-            return Response(serializer.data, status=201)
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+class GroupDetail(APIView):
+    def get(self, request, pk, format=None):
+        group = DBHandler.get_group_from_id(pk)
+        words = DBHandler.get_words_from_group(group)
+        serializer = WordSerializer(words, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        group = DBHandler.get_group_from_id(pk)
+        serializer = GroupSerializer(group, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
