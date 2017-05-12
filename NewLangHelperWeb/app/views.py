3fcbc3b18973bae636b3508c4fd0a9be207971c0
Day1721 <puzynamarek97@gmail.com>
2017-05-12 16:39:21 +0200

    corrected import

--- NewLangHelperWeb/app/views.py
+++ NewLangHelperWeb/app/views.py
@@ -4,7 +4,7 @@ from app.permissions import IsAuthenticatedOrOptions
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 # Usage (need to be logged in)
