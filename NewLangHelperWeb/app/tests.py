870a9cc8eb6cd6bb91657e53c7c3ed1a0617ab6b
Marcin Wawerka <marcin.wawerka@gmail.com>
2017-05-24 22:44:27 +0200

    Add not finished API tests

--- NewLangHelperWeb/app/tests.py
+++ NewLangHelperWeb/app/tests.py
@@ -1,34 +1,143 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-"""
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-import django
-from django.test import TestCase
 
-# TODO: Configure your database in settings.py and sync before running tests.
+class GroupAuthenticationTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='Testuser', email='test@test.com', password='Testpassword')
+        self.username = 'Testuser'
+        self.password = 'Testpassword'
+        self.url = reverse('cardgroup-list')
 
-class ViewTest(TestCase):
-    """Tests for the application views."""
+    def test_get_group_without_login(self):
+        response = self.client.get(self.url, {}, format='json')
 
-    if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
-        @classmethod
-        def setUpClass(cls):
-            super(ViewTest, cls).setUpClass()
-            django.setup()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_home(self):
-        """Tests the home page."""
-        response = self.client.get('/')
-        self.assertContains(response, 'Home Page', 1, 200)
+    def test_get_groups_with_login(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(self.url)
 
-    def test_contact(self):
-        """Tests the contact page."""
-        response = self.client.get('/contact')
-        self.assertContains(response, 'Contact', 3, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_about(self):
-        """Tests the about page."""
-        response = self.client.get('/about')
-        self.assertContains(response, 'About', 3, 200)
+    #TODO same with post?
+
+
+class GroupCreateTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='Testuser', email='test@test.com', password='Testpassword')
+        self.username = 'Testuser'
+        self.password = 'Testpassword'
+        self.url = reverse('cardgroup-list')
+        self.client.login(username=self.username, password=self.password)
+
+    def test_create_group_with_defaults(self):
+
+        data = {
+            "name": "Testgroup"
+        }
+
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data = {
+            'firstLanguage': 'PL',
+            'words': [],
+            'name': 'Testgroup',
+            'secondLanguage': 'EN'
+        }
+
+        del (response.data['pk'])
+        del (response.data['url'])
+
+        self.assertEqual(response.data, data)
+
+    def test_create_group_without_defaults(self):
+        data = {
+            "name": "Testgroup",
+            "words": [],
+            "firstLanguage": "TestLang",
+            "secondLanguage": "TestLang2",
+        }
+
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data = {
+            'firstLanguage': 'TestLang',
+            'words': [],
+            'name': 'Testgroup',
+            'secondLanguage': 'TestLang2'
+        }
+
+        del(response.data['pk'])
+        del(response.data['url'])
+
+        self.assertEqual(response.data, data)
+
+    def test_create_group_without_name(self):
+
+        data = {
+            "firstLanguage": "TestLang",
+            "secondLanguage": "TestLang2",
+        }
+
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        data = {
+            'name': ['This field is required.'],
+        }
+
+        self.assertEqual(response.data, data)
+
+    def test_create_group_with_rubbish(self):
+
+        data = {
+            'name': 'Testgroup',
+            'garbage': 'field',
+            'not': 'important'
+        }
+
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data = {
+            'firstLanguage': 'PL',
+            'words': [],
+            'name': 'Testgroup',
+            'secondLanguage': 'EN'
+        }
+
+        del (response.data['pk'])
+        del (response.data['url'])
+
+        self.assertEqual(response.data, data)
+
+    def tearDown(self):
+        self.client.logout()
+
+
+class GroupGetTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='Testuser', email='test@test.com', password='Testpassword')
+        self.username = 'Testuser'
+        self.password = 'Testpassword'
+        self.url = reverse('cardgroup-list')
+        self.client.login(username=self.username, password=self.password)
+
+    def test_get_empty_groups(self):
+        response = self.client.get(self.url, format='json')
+
+        self.assertEqual(response.data, [])
+
+    #TODO get one group/more groups
