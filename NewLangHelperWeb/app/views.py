99e162f818fc5a640c94781c53037d895ad0ac6d
Rozik <rozestwa@wp.pl>
2017-05-25 17:45:08 +0200

    commit

--- NewLangHelperWeb/app/views.py
+++ NewLangHelperWeb/app/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-
+# JAKIS KOMENTARZ
 
 # Usage (need to be logged in)
 # /groups/
